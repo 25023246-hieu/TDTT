ae66a6f5269d258017fd3d8b276227f41aac58e9
GiaHieu <25022346@vnu.edu.vn>
2025-12-01 15:59:38 +0700

    update TH5

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-#1
-n = int ( input ())
-for i in range (1 , n , 2) :
-    print (i, end = ' ')
+
+n = int(input())
+for i in range(1,n+1):
+    if i == 1 :
+        print(' '*(n-i) + '*'*i + '*'*(i-1))
+    elif i == n :
+        print(' '*(n-i) + '*'*i + '*'*(i-1))
+    else :
+        print (' '*( n - i) + '*'+' '*(i-1) + ' '*(i-2)+'*')
