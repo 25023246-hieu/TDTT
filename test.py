d843778a9295a52278bab4174997ec88c7fe536e
GiaHieu <25022346@vnu.edu.vn>
2025-12-11 17:54:53 +0700

    update hw5

--- test.py
+++ test.py
@@ -1,3 +1,30 @@
-a = [(5,1),(2,4),(3,3)]
-a.sort()
-print(a)
+#.   n=ord(n) : chuyển kí tư về số | n =chr(n) : chuyển số về kí tự
+#.   f'{ value :.nf}' : lấy sau dấu phâỷ n số
+#.   f"{value:> n kitu} : can le ben phai  n ki tu
+#.           :          :can le chinh giua
+#.           :<         : can le ben trai
+#len(s) : lấy độ dài của s
+#s.strip() : xoa ki tu khoang cach ( ' ', '\n' ,'\t')
+#s.split() : tach cac ki tu từ khoảng cách thành các phần tử con
+# dùng cho list, tuple , dict :
+                    #. a.pop(j) : xoá phần tử ở vị trí j
+                    #. a.remove(j) : xoá phânf tử đầu tiên có giá trị j
+                    #. a.append(j) : thêm giá trị j vào cuối 
+                    #. a.sort() : sắp xếp tăng đần
+                    #. a.reverse : đảo ngược list
+                    #. a.insert(i , j) : thêm phần tử j vào vị trí i trong list
+# các hàm toán tử ( k dùng import math)
+                    # round(n) : làm tròn số n 
+                    # abs(n) : giá trị tdoi của n
+                    # x**y : x mũ y
+                    # n//y : chia lấy nguyên 
+                    # a ^ b : kiểu bit của 2 số a và b
+# lệnh check lỗi 
+                    # try :
+                        # python sẽ hành đông
+                    # except ValueError :
+                        # nếu trong try gặp lỗi sẽ chạy các lệnh trong except
+                    # else :
+                        # nếu try thành công sẽ thực hiện các lệnh trong else (nếu có)
+# luu ý : return đúng kiểu dữ liệu mới đc dùng "+" , khác phải dùng ","
+                    
